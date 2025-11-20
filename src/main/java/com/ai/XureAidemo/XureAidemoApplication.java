7bf3c49c0f11a8a604dad1b88aaa4220b738cf5f
brendanobilo <brendanxure@outlook.com>
2025-11-20 14:12:20 -0500

    Initial commit (reset repo)

---
+++ src/main/java/com/ai/XureAidemo/XureAidemoApplication.java
@@ -0,0 +1,13 @@
+package com.ai.XureAidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XureAidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XureAidemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/XureAidemo/XureAidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.XureAidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XureAidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
