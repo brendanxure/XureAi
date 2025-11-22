2d51b6f468b69b9eaae79ac4c8f82975743b566e
brendanobilo <brendanxure@outlook.com>
2025-11-22 08:30:11 -0500

    update

--- src/main/java/com/ai/XureAidemo/service/ImageService.java
+++ src/main/java/com/ai/XureAidemo/service/ImageService.java
@@ -1,4 +1,9 @@
 package com.ai.XureAidemo.service;
 
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ImageService {
+    private final OpenAiImageModel openAiImageModel;
 }
