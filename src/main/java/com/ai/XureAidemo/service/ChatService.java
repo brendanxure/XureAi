c6d01b9b59ac7ea48873cd04fe02355627092635
brendanobilo <brendanxure@outlook.com>
2025-11-26 22:05:46 -0500

    update

--- src/main/java/com/ai/XureAidemo/service/ChatService.java
+++ src/main/java/com/ai/XureAidemo/service/ChatService.java
@@ -1,6 +1,7 @@
 package com.ai.XureAidemo.service;
 
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.stereotype.Service;
@@ -18,13 +19,14 @@ public class ChatService {
     }
 
     public String getResponseOptions(String prompt){
-        return chatModel.call(
-                new Prompt(prompt,
+        ChatResponse response = chatModel.call(
+                new Prompt(
+                        "Generate the names of 5 famous pirates.",
                         OpenAiChatOptions.builder()
-                                .model("gpt-4-o")
+                                .model("gpt-4o")
                                 .temperature(0.4)
                                 .build()
-                ).toString()
-        );
+                ));
+
     }
 }
