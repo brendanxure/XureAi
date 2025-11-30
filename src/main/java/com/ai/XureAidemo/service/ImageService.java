6c4d39977ea0a250f3338111085aadf4814d2622
brendanobilo <brendanxure@outlook.com>
2025-11-29 23:24:38 -0500

    update

--- src/main/java/com/ai/XureAidemo/service/ImageService.java
+++ src/main/java/com/ai/XureAidemo/service/ImageService.java
@@ -3,6 +3,7 @@ package com.ai.XureAidemo.service;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,7 +16,12 @@ public class ImageService {
 
     public ImageResponse generateImage(String prompt){
         ImageResponse imageResponse = openAiImageModel.call(
-                new ImagePrompt(prompt)
+                new ImagePrompt(prompt,
+                        OpenAiImageOptions.builder()
+                                .quality("hd")
+                                .N(4)
+                                .height(1024)
+                                .width(1024).build())
         );
         return imageResponse;
     }
