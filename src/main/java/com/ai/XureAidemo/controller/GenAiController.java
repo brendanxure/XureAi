3820194537dd9ce5ebd3958d98aa770f0b18c3af
brendanobilo <brendanxure@outlook.com>
2025-12-23 21:25:35 -0500

    update

--- src/main/java/com/ai/XureAidemo/controller/GenAiController.java
+++ src/main/java/com/ai/XureAidemo/controller/GenAiController.java
@@ -36,7 +36,7 @@ public class GenAiController {
     }
 
     /* @GetMapping("generate-image")
-    public  generateImages(HttpServletResponse) IOException {
+    public  generateImages(HttpServletResponse) IOEx {
 
     }
      */
