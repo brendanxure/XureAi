cd763c216c05ebb4e550d5c3a22044dd6c81d3f8
brendanobilo <brendanxure@outlook.com>
2026-02-26 11:52:22 -0500

    update

--- src/main/java/com/ai/XureAidemo/service/ChatService.java
+++ src/main/java/com/ai/XureAidemo/service/ChatService.java
@@ -11,7 +11,7 @@ public class ChatService {
     // Variable for the chat model
     private final ChatModel chatModel;
 
-    // Setters for chat model
+    // Parameterized Constructor
     public ChatService(ChatModel chatModel){
         this.chatModel = chatModel;
     }
